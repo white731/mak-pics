1dfdca6f0b283c82141d10fee05cee6194df427c
Landon Whitesides <whitesideslandon@gmail.com>
2021-08-21 22:27:48 -0600

    mak pics created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import BeforeAfterSlider from 'react-before-after-slider'
 
 function App() {
+
+  const before = "https://mcusercontent.com/b61bde706ca219f6690032ed4/images/848758dc-2ed4-3968-6a3a-4e5cc1533398.png"
+  const after = "https://mcusercontent.com/b61bde706ca219f6690032ed4/images/84ae4074-1c37-d885-7500-032519831dfa.png"
+
+
   return (
+  <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Makenzies Cool Pics</h1>
     </div>
+
+    <div style={{display:"flex", justifyContent: "center"}}>
+      <BeforeAfterSlider
+        before={before}
+        after = {after}
+        width={640}
+        height={480}
+      />
+    </div>
+  </>
   );
 }
 
