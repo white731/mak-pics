8ece5e63f9717df64c1a52d4c8df61b7ac535699
Landon Whitesides <whitesideslandon@gmail.com>
2021-08-21 23:02:04 -0600

    added in media query library

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import './App.css';
 import BeforeAfterSlider from 'react-before-after-slider'
+import { useMediaQuery } from 'use-media-size';
 
 function App() {
+
+  const mediaSize = useMediaQuery('(min-width:450px)')
 
   const before = "https://mcusercontent.com/b61bde706ca219f6690032ed4/images/848758dc-2ed4-3968-6a3a-4e5cc1533398.png"
   const after = "https://mcusercontent.com/b61bde706ca219f6690032ed4/images/84ae4074-1c37-d885-7500-032519831dfa.png"
@@ -17,8 +20,8 @@ function App() {
       <BeforeAfterSlider
         before={before}
         after = {after}
-        width={640}
-        height={480}
+        width={mediaSize ? 640 : 300}
+        height={mediaSize ? 480 : 225}
       />
     </div>
   </>
